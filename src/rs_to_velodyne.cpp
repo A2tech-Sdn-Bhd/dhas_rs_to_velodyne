8ec9d32a1ec488c56d1da9337aa5837070824816
mahmoud <software@a2tech.my>
2025-11-24 12:13:10 +0800

    change the intensity type from unit8 to float

--- src/rs_to_velodyne.cpp
+++ src/rs_to_velodyne.cpp
@@ -26,14 +26,14 @@ static int RING_ID_MAP_16[] = {
 // rslidar点云格式
 struct RsPointXYZIRT {
     PCL_ADD_POINT4D;
-    uint8_t intensity;
+    float intensity; // uint8_t intensity;
     uint16_t ring = 0;
     double timestamp = 0;
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 POINT_CLOUD_REGISTER_POINT_STRUCT(RsPointXYZIRT,
-                                  (float, x, x)(float, y, y)(float, z, z)(uint8_t, intensity, intensity)
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)    //(uint8_t, intensity, intensity)
                                   (uint16_t, ring, ring)(double, timestamp, timestamp))
 
 // velodyne点云格式
